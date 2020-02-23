0c3933c24a43e09cfd7767d6b1be123ec5880d99
runsen <buaa_zhangrunsen@163.com>
2020-02-23 16:37:01 +0100

    reset HEAD0.2

--- hello.php
+++ hello.php
@@ -3,4 +3,5 @@ echo 'hello, this is the second repo of runsen!'
 echo 'hello, I am coming again~'
 echo 'hello, I am coming again~'
 echo 'hello, I am coming again~, hahaha'
+echo 'hello, I am coming again~, hahahahahahah'
 ?>
